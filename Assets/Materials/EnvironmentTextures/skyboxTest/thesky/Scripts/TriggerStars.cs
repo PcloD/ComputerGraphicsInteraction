37379f54df516caab1c3e812366b245acf4ae804
icesuperbravo <icesuperbravo@icesuperbravodeMacBook-Pro.local>
2017-01-03 14:42:30 +0200

    version 1.2

---
+++ Assets/Materials/EnvironmentTextures/skyboxTest/thesky/Scripts/TriggerStars.cs
@@ -0,0 +1,25 @@
+﻿using UnityEngine;
+using System.Collections;
+
+
+public class TriggerStars : MonoBehaviour {
+
+	private ParticleSystem stars;
+	int number = 0;
+
+	// Use this for initialization
+	void Awake () { 
+		stars = GetComponent<ParticleSystem> ();
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+		if (Input.GetMouseButton (0)) 
+		{ 
+			stars.Play ();
+
+		}   
+	}
+}
